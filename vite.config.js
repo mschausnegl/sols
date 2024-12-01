af5d67bda9779ac7d80a041c8c9444b0dac4e751
mschausnegl <newsletterschau@gmail.com>
2024-12-01 01:57:47 +0100

    Restructure project for StackBlitz compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: '.',
+  base: '/',
   server: {
-    port: 3000,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        klondike: 'klondike-solitaire.html',
-        klondikeTurn3: 'klondike-turn-3.html',
-        solitaireDraw3: 'solitaire-draw-3.html'
-      }
-    }
+    port: 3000
   }
-});
+})
